99b6a49b3666299dea775aec10f4ca2e0490f7da
thatvetdevrob <thatvetdevrob@gmail.com>
2020-07-13 22:01:44 -0700

    ruber ducking the code

--- server.js
+++ server.js
@@ -42,9 +42,9 @@ app.get('/location', (request, response) => {
   //  (___/  (___/  (___/  hjw    we arrive here
   console.log('Test one');
 
-  // _      _      _
-  // >(.)__ <(.)__ =(.)__
-  //  (___/  (___/  (___/  hjw    We wrap a try catch, but no 'finally' wich would always execute
+      // _      _      _
+    // >(.)__ <(.)__ =(.)__
+    //  (___/  (___/  (___/  hjw    We wrap a try catch, but no 'finally' wich would always execute
   try{
     let city = request.query.city;
     let info = require('./data/location.json');
@@ -56,19 +56,19 @@ app.get('/location', (request, response) => {
     //i mean, we get Seattle fron the page so we have to be alright
 
     const obj = new Location(city, info);
-    // https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
-    // _      _      _
+        // https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+            // _      _      _
     // >(.)__ <(.)__ =(.)__
     //  (___/  (___/  (___/  hjw  to constructor on line 76
 
 
-    // https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
-    // _      _      _
+        // https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+            // _      _      _
     // >(.)__ <(.)__ =(.)__
     //  (___/  (___/  (___/  hjw    200 means "ok"
     response.status(200).send(obj);
   } catch(error){
-    // _      _      _
+            // _      _      _
     // >(.)__ <(.)__ =(.)__
     //  (___/  (___/  (___/  hjw    Something went wrong: i put int he wrong name or value did not match query
     console.log('something went wrong', error);
@@ -76,13 +76,13 @@ app.get('/location', (request, response) => {
   }
 });
 
-function Location(city, info){
+function Location(city, geoData){
   console.log('city constructor');
 
   this.search_query = city;
-  this.formatted_query = info[0].display_name;
-  this.latitude = info[0].lat;
-  this.longitude = info[0].lon;
+  this.formatted_query = geoData[0].display_name;
+  this.latitude = geoData[0].lat;
+  this.longitude = geoData[0].lon;
 }
 
 //=============================Weather=================================
