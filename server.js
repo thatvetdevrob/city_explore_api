2cc75ca0ba97e2ff4271c9075882c4cf4b4f433b
Rob <thatvetdevrob@gmail.com>
2020-07-13 22:16:41 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const express = require('express');
 
@@ -7,48 +7,51 @@ const app = express();
 require('dotenv').config();
 
 const cors = require('cors');
-const { response, request } = require('express');
+const { request, response } = require('express');
 
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
+// Routes
+
+//=============================Location=================================
+
 app.get('/location', (request, response) => {
+
   try{
-  let city = request.query.city;
-  let geoData = require('./data/location.json');
-  
-const obj = new Location(city, geoData);
+    let city = request.query.city;
+    let geoData = require('./data/location.json')
 
-  response.send(obj);
-} catch(error){
-  console.log('ERROR', error);
-  response.status(500).send('Sorry, somehing went wrong');
-}
+    const obj = new Location(city, geoData)
+    response.status(200).send(obj);
+  } catch(error){
+    console.log('ERROR', error);
+    response.status(500).send('Sorry, something went terribly wrong');
+  }
+})
 
-});
+function Location(city, geoData){
 
-function Location(location, geoData) {
-  this.search_query = location;
+  this.search_query = city;
   this.formatted_query = geoData[0].display_name;
   this.latitude = geoData[0].lat;
   this.longitude = geoData[0].lon;
 }
 
+//=============================Weather=================================
 
 app.get('/weather', (request, response) => {
-  let weatherInfo = require('./data/weather.json')
-  let weatherArr = [];
-  
-  weatherInfo['data'].forEach(date => {
-    weatherArr.push(new Weather(date));
-  })
-  response.send(weatherArr);
-  
-});
 
-app.use('*', (request, response) => {
-  response.status(404).send('page not found');
+  let weatherData = require('./data/weather.json')
+  let forecastArray = [];
+
+  weatherData['data'].forEach(date => {
+    forecastArray.push(new Weather(date));
+  })
+
+  response.status(200).send(forecastArray);
+
 })
 
 function Weather(obj) {
@@ -56,8 +59,14 @@ function Weather(obj) {
   this.time = obj.datetime;
 }
 
+//==============================Errors=================================
 
+app.get('*', (request, response) => {
+  response.status(500).send('Sorry, something went terribly wrong');
+})
 
+// ====================================================================
+// Turn on Server and Confirm Port
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
